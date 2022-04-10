2cc62f186631cc58b000e0ac65db0d472d582590
ngoctien70757 <ngoctien70757@gmail.com>
2022-04-10 22:04:53 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Row from './Row'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>hey Clever Programmer! Let's build Netflix Clone Front-end</h1>
+      <Row title="NETFLIX ORIGINALS" />
+      <Row title="Trending Now" />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Row.js
@@ -0,0 +1,13 @@
+
+
+function Row({ title }){
+    return(
+        <div>
+            <h2>{title}</h2>
+
+            {/* container porter*/}
+        </div>
+    )
+}
+
+export default Row
